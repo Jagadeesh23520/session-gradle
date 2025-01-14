06a3395b99036e563c08d166e3edae04793cef69
Jagadeesh23520 <jagadeeshp0007@gmail.com>
2025-01-14 20:49:54 +0530

    update session redis cache

--- src/main/java/com/example/session/controller/SessionControllerAPI.java
+++ src/main/java/com/example/session/controller/SessionControllerAPI.java
@@ -3,11 +3,12 @@ package com.example.session.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.session.model.UserCredentails;
+import com.example.session.model.UserDetails;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -17,13 +18,13 @@ public interface SessionControllerAPI {
 	@PostMapping(value = "/login")
 	public ResponseEntity<Object> getLogin(HttpServletRequest request, @RequestBody UserCredentails userCredentails);
 
-//	@PostMapping(value = "/register")
-//	public ResponseEntity<Object> createUser(HttpServletRequest request, @RequestBody UserCredentails userCredentails);
+	@PostMapping(value = "/register")
+	public ResponseEntity<Object> createUser(HttpServletRequest request, @RequestBody UserDetails userDetails);
 
 	@GetMapping(value = "/userlogin")
-	public void loginByUserName(HttpServletRequest request);
+	public ResponseEntity<String> loginByUserName(HttpServletRequest request);
 
-	@GetMapping(value = "/reg")
-	public void reg(@RequestParam(required = true) String test);
+	@PutMapping(value = "/updateUsers")
+	public ResponseEntity<Object> updateUserDetails(@RequestBody UserDetails userDetails);
 
 }
